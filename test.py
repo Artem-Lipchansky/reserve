93468a2fcc0c23f7641f5db84b93f81f250012ae
Artem Lipchansky <lipchanskiy.an@gmail.com>
2025-11-30 04:55:01 +0200

    add

---
+++ test.py
@@ -0,0 +1,28 @@
+name_1 = 100
+name_2 = 20
+
+name_3 = name_1 + name_2
+
+print(name_3)
+
+name_1 = 100
+name_2 = 20
+
+name_3 = name_1 -name_2
+
+print(name_3)
+
+name_1 = 100
+name_2 = 20
+
+name_3 = name_1 * name_2
+
+print(name_3)
+
+name_1 = 100
+name_2 = 20
+
+name_3 = name_1 / name_2
+
+print(name_3)
+
